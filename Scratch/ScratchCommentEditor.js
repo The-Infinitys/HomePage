6a5ac59fd6befabb92c876073908af14f21e2178
The-Infinitys <137460810+The-Infinitys@users.noreply.github.com>
2024-01-04 10:18:32 +0900

    a

---
+++ Scratch/ScratchCommentEditor.js
@@ -0,0 +1,146 @@
+//ScratchCommentEditor
+//htmlのソース
+/*
+<div class="flex-row comments-list">
+                    <div class="flex-row comment-container">
+                      <div class="flex-row comment" id="comments-376346782">
+                        <a href="/users/The_Infinitys">
+                          <img class="avatar" src="https://cdn2.scratch.mit.edu/get_image/user/116605060_60x60.png">
+                        </a>
+                        <div class="flex-row comment-body column">
+                          <div class="flex-row comment-top-row">
+                            <a class="username" href="/users/The_Infinitys">The_Infinitys</a>
+                            <div class="action-list">
+                              <span class="comment-delete">
+                                <span>削除</span>
+                              </span>
+                              <span class="comment-report">
+                                <span>報告</span>
+                              </span>
+                            </div>
+                          </div>
+                          <div class="comment-bubble">
+                            <span class="comment-content">
+                              <span class="emoji-text">Welcome 2 </span>
+                              <a href="/users/The_Infinitys">@The_Infinitys</a>
+                              <span class="emoji-text">! 
+                                <img src="/images/emoji/gobo.png" class="emoji" alt="gobo emoji">
+                              </span>
+                            </span>
+                            <div class="flex-row comment-bottom-row">
+                              <span class="comment-time">
+                                <span>12 時間前</span>
+                              </span>
+                              <span class="comment-reply">
+                                <span>返信</span>
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="flex-row comment-container">
+                      <div class="flex-row comment" id="comments-376346199">
+                        <a href="/users/The_Infinitys">
+                          <img class="avatar" src="https://cdn2.scratch.mit.edu/get_image/user/116605060_60x60.png">
+                        </a>
+                        <div class="flex-row comment-body column">
+                          <div class="flex-row comment-top-row">
+                            <a class="username" href="/users/The_Infinitys">The_Infinitys</a>
+                            <div class="action-list">
+                              <span class="comment-delete">
+                                <span>削除</span>
+                              </span>
+                              <span class="comment-report">
+                                <span>報告</span>
+                              </span>
+                            </div>
+                          </div>
+                          <div class="comment-bubble">
+                            <span class="comment-content">
+                              <span class="emoji-text">test</span>
+                            </span>
+                            <div class="flex-row comment-bottom-row">
+                              <span class="comment-time">
+                                <span>12 時間前</span>
+                              </span>
+                              <span class="comment-reply">
+                                <span>返信</span>
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="flex-row replies column">
+                        <div class="flex-row comment" id="comments-376473737">
+                          <a href="/users/The_Infinitys">
+                            <img class="avatar" src="https://cdn2.scratch.mit.edu/get_image/user/116605060_60x60.png">
+                          </a>
+                          <div class="flex-row comment-body column">
+                            <div class="flex-row comment-top-row">
+                              <a class="username" href="/users/The_Infinitys">The_Infinitys</a>
+                              <div class="action-list">
+                                <span class="comment-delete">
+                                  <span>削除</span>
+                                </span>
+                                <span class="comment-report">
+                                  <span>報告</span>
+                                </span>
+                              </div>
+                            </div>
+                            <div class="comment-bubble">
+                              <span class="comment-content">
+                                <span class="emoji-text"></span>
+                                <a href="/users/The_Infinitys">@The_Infinitys</a>
+                                <span class="emoji-text">reply</span>
+                              </span>
+                              <div class="flex-row comment-bottom-row">
+                                <span class="comment-time">
+                                  <span>今</span>
+                                </span>
+                                <span class="comment-reply">
+                                  <span>返信</span>
+                                </span>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+*/
+class ScratchCommentEditor{
+  constructor(){
+    this.emoji=[
+      '_meow_',
+      '_gobo_',
+      '_10mil_',
+      '_apple_',
+      '_waffle_',
+      '_camera_',
+      '_taco_',
+      '_sushi_',
+      '_broccoli_',
+      '_pizza_',
+      '_candycorn_',
+      '_map_',
+      '_compass_',
+      '_suitcase_',
+      '_binoculars_',
+      '_B)_',
+      '_:)_',
+      '_:D_',
+      '_:P_',
+      '_;P_',
+      '_:P_',
+      '_P:_',
+      '_:3_',
+      '_<3_',
+      '_**_',
+      '_:))_',
+      '_:D<_',
+      '_BLM_',
+      '_pride_'
+    ]
+  }
+}
