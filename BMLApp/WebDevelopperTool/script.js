6558b5e0839fb54b2dbddb1392de5199fbd9437c
The-Infinitys <137460810+The-Infinitys@users.noreply.github.com>
2024-01-05 12:44:56 +0900

    s

--- BMLApp/WebDevelopperTool/script.js
+++ BMLApp/WebDevelopperTool/script.js
@@ -1,9 +1,9 @@
 document.querySelector('#VisibleSwitch').addEventListener('click', (e) => {
-    if(document.getElementById('DevelopperToolBox').style.visibility==='visible'){
-      document.getElementById('DevelopperToolBox').style.visibility = 'hidden';
-      document.getElementById('VisibleSwitch').style.opacity='0';
-    }else{
-      document.getElementById('DevelopperToolBox').style.visibility = 'visible';
-      document.getElementById('VisibleSwitch').style.opacity='1';
-    }
-  });
+  if(document.getElementById('DevelopperToolBox').style.visibility==='visible'){
+    document.getElementById('DevelopperToolBox').style.visibility = 'hidden';
+    document.getElementById('VisibleSwitch').style.opacity='0';
+  }else{
+    document.getElementById('DevelopperToolBox').style.visibility = 'visible';
+    document.getElementById('VisibleSwitch').style.opacity='1';
+  }
+});
