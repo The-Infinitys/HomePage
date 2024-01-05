e82417eef11beb77e399330e88c9de164829436a
The-Infinitys <137460810+The-Infinitys@users.noreply.github.com>
2024-01-05 13:57:01 +0900

    z

--- BMLApp/WebDevelopperTool/script.js
+++ BMLApp/WebDevelopperTool/script.js
@@ -1,3 +1,7 @@
+var HTMLEditor=document.getElementById("HTMLEditor")
+var CSSEditor=document.getElementById("CSSEditor")
+var JSEditor=document.getElementById("JSEditor")
+
 document.querySelector('#VisibleSwitch').addEventListener('click', (e) => {
   if(document.getElementById('DevelopperToolBox').style.visibility==='visible'){
     document.getElementById('DevelopperToolBox').style.visibility = 'hidden';
@@ -5,5 +9,28 @@ document.querySelector('#VisibleSwitch').addEventListener('click', (e) => {
   }else{
     document.getElementById('DevelopperToolBox').style.visibility = 'visible';
     document.getElementById('VisibleSwitch').style.opacity='1';
+  }
+});
+document.querySelector('#HTML').addEventListener('click', (e) => {
+  if(HTMLEditor.style.visibility==='Hidden'){
+    CSSEditor.style.visibility='Hidden';
+    JSEditor.style.visibility='Hidden';
+    HTMLEditor.style.visibility='Visible';
+  }
+});
+
+document.querySelector('#CSS').addEventListener('click', (e) => {
+  if(HTMLEditor.style.visibility==='Hidden'){
+    HTMLEditor.style.visibility='Hidden';
+    JSEditor.style.visibility='Hidden';
+    CSSEditor.style.visibility='Visible';
+  }
+});
+
+document.querySelector('#JS').addEventListener('click', (e) => {
+  if(HTMLEditor.style.visibility==='Hidden'){
+    CSSEditor.style.visibility='Hidden';
+    HTMLEditor.style.visibility='Hidden';
+    JSEditor.style.visibility='Visible';
   }
 });
