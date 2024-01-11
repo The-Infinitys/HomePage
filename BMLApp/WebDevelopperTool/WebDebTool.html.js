910574b4bf12168192cb249df3f19b85bdb23051
The-Infinitys <137460810+The-Infinitys@users.noreply.github.com>
2024-01-11 13:26:28 +0900

    s

--- BMLApp/WebDevelopperTool/WebDebTool.html.js
+++ BMLApp/WebDevelopperTool/WebDebTool.html.js
@@ -2,7 +2,7 @@ if (document.getElementById("InfinityDevelopperTool") == null) {
   let IDThtml = document.createElement("div");
   IDThtml.id = "InfinityDevelopperTool";
   IDThtml.innerHTML = `
-  `;
+`;
   document.body.appendChild(IDThtml);
 } else {
 
