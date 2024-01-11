6cd70398d9291ebcddb3421ae82a38bad8c0fd91
The-Infinitys <137460810+The-Infinitys@users.noreply.github.com>
2024-01-11 09:48:59 +0900

    s

--- BMLApp/WebDevelopperTool/script.js
+++ BMLApp/WebDevelopperTool/script.js
@@ -8,15 +8,15 @@ function getHTMLSource() {
     .replace("/</g", "&lt;")
     .replace("/>/g", "&gt;");
 }
-function openDevelopper(){
-    document.getElementById("DevelopperToolBox").style.visibility = "visible";
-    
+function openDevelopper() {
+  document.getElementById("DevelopperToolBox").style.visibility = "visible";
+
   document.getElementById("MaximiseSwitch").style.visibility = "visible";
-    
+
   CSSEditor.style.visibility = "hidden";
   JSEditor.style.visibility = "hidden";
   HTMLEditor.style.visibility = "visible";
-    document.getElementById("VisibleSwitch").style.opacity = "1";
+  document.getElementById("VisibleSwitch").style.opacity = "1";
 }
 openDevelopper();
 HTMLSource.value = getHTMLSource();
@@ -25,20 +25,20 @@ document.querySelector("#VisibleSwitch").addEventListener("click", (e) => {
     document.getElementById("DevelopperToolBox").style.visibility === "visible"
   ) {
     document.getElementById("DevelopperToolBox").style.visibility = "hidden";
-  document.getElementById("MaximiseSwitch").style.visibility = "hidden";
-  CSSEditor.style.visibility = "hidden";
-  JSEditor.style.visibility = "hidden";
-  HTMLEditor.style.visibility = "hidden";
+    document.getElementById("MaximiseSwitch").style.visibility = "hidden";
+    CSSEditor.style.visibility = "hidden";
+    JSEditor.style.visibility = "hidden";
+    HTMLEditor.style.visibility = "hidden";
     document.getElementById("VisibleSwitch").style.opacity = "0";
   } else {
     openDevelopper();
   }
 });
-document.querySelector("#MaximiseSwitch").addEventListener("click",(e)=>{
-  if(document.getElementById("DevelopperToolBox").style.width=="100%"){
-    document.getElementById("DevelopperToolBox").style.width="20%";
-  }else{
-    document.getElementById("DevelopperToolBox").style.width="100%";
+document.querySelector("#MaximiseSwitch").addEventListener("click", (e) => {
+  if (document.getElementById("DevelopperToolBox").style.width == "100%") {
+    document.getElementById("DevelopperToolBox").style.width = "20%";
+  } else {
+    document.getElementById("DevelopperToolBox").style.width = "100%";
   }
 })
 document.querySelector("#HTML").addEventListener("click", (e) => {
@@ -64,3 +64,16 @@ document.querySelector("#UpdateHTML").addEventListener("click", (e) => {
   document.open();
   document.write(HTMLSource.value);
 });
+console.log = ((logTextAreaArgument) => {
+  let logTextArea = logTextAreaArgument;
+  return text => logTextArea.innerHTML += text + '<br>';
+})(document.getElementById("JSRunnerConsole"));
+function a() {
+  x = document.getElementById("test")
+  x.remove();
+  y = document.createElement("script");
+  y.id = "test";
+  y.innerHTML = "console.log(" + getRandomInt(9).toString() + ")";
+  document.body.appendChild(y);
+  document.getElementById("123").innerHTML = console;
+}
