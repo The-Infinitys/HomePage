fbc8eeb4ef64464eff1a133113f4120fb8a12d92
The-Infinitys <137460810+The-Infinitys@users.noreply.github.com>
2024-02-08 12:04:13 +0900

    ctvfgyb

--- InfinityVideoEditor/script.js
+++ InfinityVideoEditor/script.js
@@ -1,5 +1,8 @@
-var importedSources=[]
-function importFile(){
-    
+var importedSources = [];
+function importFile() {
+  let dialoger = document.createElement("input");
+  dialoger.type = "file";
+  dialoger.click();
+
 }
-document.getElementById("left-button-importFile").addEventListener("click",importFile())
+document.getElementById("left-button-importFile").addEventListener("click", importFile());
