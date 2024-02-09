7a65fc8b657f28d6011a23f2484641ed14f0dea8
The-Infinitys <137460810+The-Infinitys@users.noreply.github.com>
2024-02-09 09:22:18 +0900

    sdcjn

--- InfinityVideoEditor/script.js
+++ InfinityVideoEditor/script.js
@@ -1,9 +1,13 @@
 var importedSources = [];
 
 document.getElementById("left-button-importFile").addEventListener("click", (e)=>{
-    //ファイルをimportする為の関数
-    let dialoger = document.createElement("input");
-    dialoger.type = "file";
+    //ソース(動画、画像、音声)をimportする為にボタンを押させる関数
+    let dialoger = document.createElement("file-dialog-source");
     dialoger.click();
+});
 
+document.getElementById("left-button-importProject").addEventListener("click", (e)=>{
+    //プロジェクトをimportする為にボタンを押させる関数
+    let dialoger = document.createElement("file-dialog-project");
+    dialoger.click();
 });
