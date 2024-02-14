813ba76d7c41dc210549317b76a6e6d563d1a6e8
The-Infinitys <137460810+The-Infinitys@users.noreply.github.com>
2024-02-14 13:07:05 +0900

    add sprite const value.

--- InfinityVideoEditor/script.js
+++ InfinityVideoEditor/script.js
@@ -1,13 +1,69 @@
+//メモリ取得(プロジェクト用)
 var importedSources = [];
+var ProjectJSON = {
+  title: "",
+  sources: [], //here is the name of source file
+  sprites: {},
+};
 
-document
-  .getElementById("editor-button-importFile")
-  .addEventListener("click", (e) => {
-    //ソース(動画、画像、音声)をimportする為にボタンを押させる関数
-    let dialoger = document.getElementById("file-dialog-source");
-    dialoger.click();
-  });
+//定数の生成(スプライトの雛形)
+const sprite_animation = {
+  persent: 0, //it needs 0~100.
+  x: 0,
+  y: 0,
+  direction: 0,
+  rotation_way: "right", //it needs left or right
+  size: 100,
+};
+const sprite_video = {
+  name: "",
+  source: {
+    name: "",
+    start: 0,
+    speed: 1,
+  },
+  start_time: 0,
+  duration: 0,
+  position: {
+    x: 0,
+    y: 0,
+    direction: 0,
+    size: 100,
+  },
+  animation: [],
+};
 
+const sprite_image = {
+  name: "",
+  source: "",
+  start_time: 0,
+  duration: 0,
+  position: {
+    x: 0,
+    y: 0,
+    direction: 0,
+    size: 100,
+  },
+  animation: [],
+};
+
+const sprite_text = {
+  name: "",
+  source: {
+    text: "",
+    font: "",
+  },
+  start_time: 0,
+  duration: 0,
+  position: {
+    x: 0,
+    y: 0,
+    direction: 0,
+    size: 100,
+  },
+  animation: [],
+};
+//ファイルの読み込み
 document
   .getElementById("file-dialog-source")
   .addEventListener("change", (e) => {
@@ -20,17 +76,26 @@ document
   });
 
 document
+  .getElementById("file-dialog-project")
+  .addEventListener("change", (e) => {
+    alert("現在作成中です...");
+  });
+
+//画面上の動きを作る。
+document
+  .getElementById("editor-button-importFile")
+  .addEventListener("click", (e) => {
+    //ソース(動画、画像、音声)をimportする為にボタンを押させる関数
+    let dialoger = document.getElementById("file-dialog-source");
+    dialoger.click();
+  });
+
+document
   .getElementById("editor-button-importProject")
   .addEventListener("click", (e) => {
     //プロジェクトをimportする為にボタンを押させる関数
     let dialoger = document.getElementById("file-dialog-project");
     dialoger.click();
-  });
-
-document
-  .getElementById("file-dialog-project")
-  .addEventListener("change", (e) => {
-    alert("現在作成中です...");
   });
 
 document.getElementById("editor-back").addEventListener("click", (e) => {
