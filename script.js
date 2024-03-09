ce899f68a519ab940084f7508d23d5972e8c0937
The-Infinitys <137460810+The-Infinitys@users.noreply.github.com>
2024-03-09 10:24:30 +0900

    fixed bugs about...?

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
 const darkModeOn = darkModeMediaQuery.matches;
 
-const startLogo=document.querySelector("startLogo");
-const logo=document.querySelector("logo");
-if(darkModeOn){
-}else{
-  startLogo.src="/images/TheInfinitys(White).png";
-  logo.src="/images/Infinity Light.svg";
-}
+const startLogo = document.querySelector("startLogo");
+const logo = document.querySelector("logo");
+setInterval(() => {
+  if (darkModeOn) {
+  } else {
+    startLogo.src = "/images/TheInfinitys(White).png";
+    logo.src = "/images/Infinity Light.svg";
+  }
+}, 1000);
