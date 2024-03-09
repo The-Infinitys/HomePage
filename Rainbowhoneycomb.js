4ed9b535a7dc225d2f1688a453c3723eba5bfab0
The-Infinitys <137460810+The-Infinitys@users.noreply.github.com>
2024-03-09 09:48:49 +0900

    ダークモード・らいともーど対応

--- Rainbowhoneycomb.js
+++ Rainbowhoneycomb.js
@@ -1,3 +1,5 @@
+//ダークモード・ライトモード対応
+
 const root3 = 1.7320508;
 const RainbowHoneycomb = document.getElementById("RainbowHoneycomb");
 function resizeCanvas() {
@@ -6,23 +8,35 @@ function resizeCanvas() {
 }
 resizeCanvas();
 window.onresize = resizeCanvas;
-RainbowHoneycomb.style = "position:fixed;width:100%;height:100%;top:0;left:0;z-index:-100;"
+RainbowHoneycomb.style =
+  "position:fixed;width:100%;height:100%;top:0;left:0;z-index:-100;";
 const draw = RainbowHoneycomb.getContext("2d");
 function honeycomb(x, y, r) {
-  draw.fillStyle = "black";
+  const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+  const darkModeOn = darkModeMediaQuery.matches;
+  if (darkModeOn) {
+    draw.fillStyle = "black";
+  } else {
+    draw.fillStyle = "white";
+  }
   draw.beginPath();
   draw.moveTo(x, y - r);
-  draw.lineTo(x + root3 / 2 * r, y - r / 2);
-  draw.lineTo(x + root3 / 2 * r, y + r / 2);
+  draw.lineTo(x + (root3 / 2) * r, y - r / 2);
+  draw.lineTo(x + (root3 / 2) * r, y + r / 2);
   draw.lineTo(x, y + r);
-  draw.lineTo(x - root3 / 2 * r, y + r / 2);
-  draw.lineTo(x - root3 / 2 * r, y - r / 2);
+  draw.lineTo(x - (root3 / 2) * r, y + r / 2);
+  draw.lineTo(x - (root3 / 2) * r, y - r / 2);
   draw.fill();
 }
 const radius = 50;
 var hsvToRgb16 = function (hue, saturation, value) {
   var result = false;
-  if (((saturation || saturation === 0) && saturation <= 100) && ((value || value === 0) && value <= 100)) {
+  if (
+    (saturation || saturation === 0) &&
+    saturation <= 100 &&
+    (value || value === 0) &&
+    value <= 100
+  ) {
     var red = 0,
       green = 0,
       blue = 0,
@@ -80,7 +94,7 @@ var hsvToRgb16 = function (hue, saturation, value) {
     result = {
       red: Math.round(red * 255).toString(),
       green: Math.round(green * 255).toString(),
-      blue: Math.round(blue * 255).toString()
+      blue: Math.round(blue * 255).toString(),
     };
   }
   return "rgb(" + result.red + "," + result.green + "," + result.blue + ")";
@@ -88,12 +102,20 @@ var hsvToRgb16 = function (hue, saturation, value) {
 var tickcount = 0;
 function drawhoneycomb() {
   for (let i = 0; i < RainbowHoneycomb.width; ++i) {
-    draw.fillStyle = hsvToRgb16(i * 360 / RainbowHoneycomb.width + tickcount, 100, 100);
+    draw.fillStyle = hsvToRgb16(
+      (i * 360) / RainbowHoneycomb.width + tickcount,
+      100,
+      100
+    );
     draw.fillRect(i, 0, 1, RainbowHoneycomb.height);
   }
   for (let i = 0; i < Math.round(RainbowHoneycomb.width / radius) + 2; ++i) {
     for (let j = 0; j < Math.round(RainbowHoneycomb.height / radius) + 2; ++j) {
-      honeycomb(radius * 2 * i + (j % 2) * radius, radius * 2 * root3 / 2 * j, radius);
+      honeycomb(
+        radius * 2 * i + (j % 2) * radius,
+        ((radius * 2 * root3) / 2) * j,
+        radius
+      );
     }
   }
   tickcount += 1;
