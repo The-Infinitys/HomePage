f3556239741fc72fbfea82db4c66096b9e29531d
The-Infinitys <137460810+The-Infinitys@users.noreply.github.com>
2024-03-19 15:07:07 +0900

    多分治った

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-const darkModeOn = darkModeMediaQuery.matches;
-
-const startLogo = document.querySelector("startLogo");
-const logo = document.querySelector("logo");
+const startLogo = document.querySelector("#startLogo");
+const logo = document.querySelector("#logo");
 setInterval(() => {
+  const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+  const darkModeOn = darkModeMediaQuery.matches;
+
   if (darkModeOn) {
+    startLogo.src = "./images/TheInfinitys(Black).png";
   } else {
-    startLogo.src = "/images/TheInfinitys(White).png";
-    logo.src = "/images/Infinity Light.svg";
+    startLogo.src = "./images/TheInfinitys(White).png";
   }
 }, 1000);
