81f4455a2270d03fb76ba85c4b8f04510435174a
The-Infinitys <137460810+The-Infinitys@users.noreply.github.com>
2024-04-16 09:29:09 +0900

    RESET

---
+++ script.js
@@ -0,0 +1,12 @@
+const startLogo = document.querySelector("#startLogo");
+const logo = document.querySelector("#logo");
+setInterval(() => {
+  const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+  const darkModeOn = darkModeMediaQuery.matches;
+
+  if (darkModeOn) {
+    startLogo.src = "/images/TheInfinitys(Black).png";
+  } else {
+    startLogo.src = "/images/TheInfinitys(White).jpeg";
+  }
+}, 100);
