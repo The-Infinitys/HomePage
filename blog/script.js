3c5f92d6c3bf87dc258f8aec4d2f803e80568e02
The-Infinitys <137460810+The-Infinitys@users.noreply.github.com>
2024-04-19 04:50:16 +0000

    取り敢えず途中まで完成

--- blog/script.js
+++ blog/script.js
@@ -1,25 +1,26 @@
-result={};
+result = {};
 const getData = function (count) {
   fetch("https://the-infinitys.f5.si/BLOG/list/" + count.toString() + ".json")
     .then((res) => res.json())
     .then((apiData) => {
-      const info=apiData.info;
-      console.log(info);
-      const box=document.createElement("button");
-      box.className="blog-button";
-      box.onclick=()=>{
-        blog_open(info["html"]);
-      }
-      const thumbnail=document.createElement("img");
-      thumbnail.src="https://the-infinitys.f5.si/BLOG"+info["thumbnail"];
-      thumbnail.alt=info["name"];
-      const title=document.createElement("div");
-      title.innerHTML=info["title"];
-      box.innerHTML=thumbnail.outerHTML+title.outerHTML;
-      document.querySelector(".list div").appendChild(box);
+      const infos = apiData.info;
+      infos.forEach(info => {
+        const box = document.createElement("button");
+        box.className = "blog-button";
+        box.onclick = () => {
+          blog_open(info.html);
+        }
+        const thumbnail = document.createElement("img");
+        thumbnail.src = "https://the-infinitys.f5.si/BLOG" + info.thumbnail;
+        thumbnail.alt = info.name;
+        const title = document.createElement("div");
+        title.innerHTML = info.title;
+        box.innerHTML = thumbnail.outerHTML + title.outerHTML;
+        document.querySelector(".list").appendChild(box);
+      });
     }).catch((err) => console.log(`データが取得できませんでした：${err}`));
 };
-function blog_open(url){
+function blog_open(url) {
 
 }
 getData(1);
