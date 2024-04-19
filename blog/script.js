f81009187980d4ebd783fbf1c9381e828c99a9dd
The-Infinitys <137460810+The-Infinitys@users.noreply.github.com>
2024-04-19 04:45:05 +0000

    途中まで

--- blog/script.js
+++ blog/script.js
@@ -1,8 +1,25 @@
-const getData = function () {
-  fetch("https://the-infinitys.f5.si/blog-data/list/1.json")
+result={};
+const getData = function (count) {
+  fetch("https://the-infinitys.f5.si/BLOG/list/" + count.toString() + ".json")
     .then((res) => res.json())
     .then((apiData) => {
-      alert(apiData);
-    }).catch((err) => console.error(`データが取得できませんでした：${err}`));
+      const info=apiData.info;
+      console.log(info);
+      const box=document.createElement("button");
+      box.className="blog-button";
+      box.onclick=()=>{
+        blog_open(info["html"]);
+      }
+      const thumbnail=document.createElement("img");
+      thumbnail.src="https://the-infinitys.f5.si/BLOG"+info["thumbnail"];
+      thumbnail.alt=info["name"];
+      const title=document.createElement("div");
+      title.innerHTML=info["title"];
+      box.innerHTML=thumbnail.outerHTML+title.outerHTML;
+      document.querySelector(".list div").appendChild(box);
+    }).catch((err) => console.log(`データが取得できませんでした：${err}`));
 };
-getData();
+function blog_open(url){
+
+}
+getData(1);
