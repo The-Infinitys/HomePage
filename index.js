ce6beb3975d766c531d41be65398e5266cb2ad09
The-Infinitys <137460810+The-Infinitys@users.noreply.github.com>
2024-04-22 11:52:38 +0900

    取り敢えず完成

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ setInterval(() => {
   const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
   const darkModeOn = darkModeMediaQuery.matches;
   if (darkModeOn) {
-    black.style.visibility="visible";
-    white.style.visibility="hidden";
+    black.style.visibility = "visible";
+    white.style.visibility = "hidden";
   } else {
-    black.style.visibility="hidden";
-    white.style.visibility="visible";
+    black.style.visibility = "hidden";
+    white.style.visibility = "visible";
   }
 }, 100);
+
+let is_opened_hamburgerMenu = false;
+let hamburger_menu = () => {
+  is_opened_hamburgerMenu = !is_opened_hamburgerMenu;
+  const menu = document.querySelector("#hamburger_menus");
+  if (is_opened_hamburgerMenu) {
+    menu.style.visibility = "visible";
+  } else {
+    menu.style.visibility = "hidden";
+  }
+};
