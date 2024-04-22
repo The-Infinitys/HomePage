3b547e908d8a6972fff41c02ab4ecd53fd0a3c27
The-Infinitys <137460810+The-Infinitys@users.noreply.github.com>
2024-04-23 08:38:57 +0900

    こんな感じ

--- blog/script.js
+++ blog/script.js
@@ -6,7 +6,7 @@ const getData = function (count) {
       const infos = apiData.info;
       infos.forEach(info => {
         const box = document.createElement("button");
-        box.className = "blog-button";
+        box.classList.add("blog-button");
         box.onclick = () => {
           document.querySelector("#blog-title").innerHTML=info.title;
           blog_open("https://the-infinitys.f5.si/BLOG" + info.html);
