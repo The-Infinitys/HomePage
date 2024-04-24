80974d705f6a8b0232e13f595208720d0b1c74fd
The-Infinitys <137460810+The-Infinitys@users.noreply.github.com>
2024-04-24 09:14:48 +0900

    swの更新

--- PWA/sw.js
+++ PWA/sw.js
@@ -1,9 +1,5 @@
-/*
-==============================================
-Thank you for Mozzila!
-==============================================
-https://developer.mozilla.org/ja/docs/Web/Progressive_web_apps/Tutorials/js13kGames/Offline_Service_workers
-*/
+const staticCacheName = 'The-Infinitys-cache'; // 独自のキャッシュ名を設定
+const offlineImage = '/image/loading-infinity.svg'; // 代替画像のパス
 
 //when installed
 self.addEventListener("install", (e) => {
@@ -15,23 +11,21 @@ self.addEventListener("install", (e) => {
   console.log("The-Infinitys' [Service Worker] activated");
 });
 
-//動的キャッシュ
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((r) => {
-      console.log("[Service Worker] Fetching resource: " + e.request.url);
-      return (
-        r ||
-        fetch(e.request).then((response) => {
-          return caches.open(cacheName).then((cache) => {
-            console.log(
-              "[Service Worker] Caching new resource: " + e.request.url,
-            );
-            cache.put(e.request, response.clone());
-            return response;
-          });
-        })
-      );
-    }),
-  );
+self.addEventListener('fetch', (event) => {
+  // Check if this is a navigation request
+  if (event.request.mode === 'navigate') {
+    // Open the cache
+    event.respondWith(caches.open(cacheName).then((cache) => {
+      // Go to the network first
+      return fetch(event.request.url).then((fetchedResponse) => {
+        cache.put(event.request, fetchedResponse.clone());
+        return fetchedResponse;
+      }).catch(() => {
+        // If the network is unavailable, get
+        return cache.match(event.request.url);
+      });
+    }));
+  } else {
+    return cache.match(offlineImage);
+  }
 });
