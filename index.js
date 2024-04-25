012093f84f585dd190f72724b5c03ff500a9c918
The-Infinitys <137460810+The-Infinitys@users.noreply.github.com>
2024-04-25 02:19:22 +0000

    なぜかうまくいかない

--- index.js
+++ index.js
@@ -3,10 +3,29 @@ const init_header = function () {
   fetch("/templete/header.html")
     .then((res) => res.text())
     .then((header) => {
-      document.querySelector("header").innerHTML=header;
+      document.querySelector("header").innerHTML = header;
     }).catch((err) => console.log(`データが取得できませんでした：${err}`));
 };
 
-window.onload= () =>{
+window.onload = () => {
   init_header();
+}
+
+
+//ハンバーガーメニューの設定
+let is_opened_hamburgerMenu = false;
+function hamburger_menu() {
+  is_opened_hamburgerMenu = !is_opened_hamburgerMenu;
+  const menu = document.querySelector("#hamburger_menus");
+  if (is_opened_hamburgerMenu) {
+    menu.classList.add("hamburger-open");
+    menu.classList.remove("hamburger-close");
+    menu.style.width = "35%";
+    menu.style.opacity = "1";
+  } else {
+    menu.classList.add("hamburger-close");
+    menu.classList.remove("hamburger-open");
+    menu.style.width = "0";
+    menu.style.opacity = "0";
+  }
 }
