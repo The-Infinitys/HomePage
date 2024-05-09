85a90dbd683415df43dc8f9c9818b14cfe896ae7
The-Infinitys <137460810+The-Infinitys@users.noreply.github.com>
2024-05-09 11:14:14 +0900

    色々変更

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 //headerの初期化
+const header = `
+<a href="/"
+  ><img id="headerLogo-light" src="/image/TheInfinitys_Light.webp" alt="" />
+  <img id="headerLogo-dark" src="/image/TheInfinitys_Dark.webp" alt="" />
+</a>
+<h1>The Infinity's</h1>
+<button
+  id="hamburger_menu_button"
+  aria-label="hamburger_menu_button"
+  onclick="hamburger_menu()">
+  <svg
+    id="hamburger_menu_icon"
+    viewBox="0 0 100 100"
+    fill="none"
+    stroke-width="10">
+    <line x1="10" y1="20" x2="90" y2="20" />
+    <line x1="10" y1="50" x2="90" y2="50" />
+    <line x1="10" y1="80" x2="90" y2="80" />
+  </svg>
+</button>
+<!--hamburger menu-->
+<div id="hamburger_menus">
+  <div><a href="/blog">Blog</a></div>
+  <div><a href="https://scratch.mit.edu/users/The_Infinitys">Scratch</a></div>
+  <div><a href="https://github.com/The_Infinitys">GitHub</a></div>
+  <div><a href="/">Home</a></div>
+</div>
+`;
 const init_header = function () {
-  fetch("/templete/header.html")
-    .then((res) => res.text())
-    .then((header) => {
-      document.querySelector("header").innerHTML = header;
-    }).catch((err) => console.log(`データが取得できませんでした：${err}`));
+  document.querySelector("header").innerHTML = header;
 };
 
 window.onload = () => {
   init_header();
 }
-
-
 //ハンバーガーメニューの設定
 let is_opened_hamburgerMenu = false;
 function hamburger_menu() {
