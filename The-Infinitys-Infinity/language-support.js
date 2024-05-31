88dbbb673cefde93bc7bfbbba6d1f202bf261bc7
The Infinitys <137460810+The-Infinitys@users.noreply.github.com>
2024-05-31 12:45:26 +0900

    test

--- The-Infinitys-Infinity/language-support.js
+++ The-Infinitys-Infinity/language-support.js
@@ -4,6 +4,7 @@ const translate_Infinitys = (lang, data) => {
     for (let i = 0; i < data.length; ++i) {
       const elem = document.querySelector(translate_info[i]["elem"]);
       elem.innerHTML = translate_info[i]["text"];
+      alert(JSON.stringify(translate_info));
     }
   }
 };
