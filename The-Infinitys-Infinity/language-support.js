28b7279b3ca2006c3cc1a8ad6ed1e93ec484b3ab
The Infinitys <137460810+The-Infinitys@users.noreply.github.com>
2024-05-31 12:49:14 +0900

    バグ修正

--- The-Infinitys-Infinity/language-support.js
+++ The-Infinitys-Infinity/language-support.js
@@ -1,10 +1,9 @@
 const translate_Infinitys = (lang, data) => {
   if (lang in data) {
     const translate_info = data[lang];
-    for (let i = 0; i < data.length; ++i) {
+    for (let i = 0; i < translate_info.length; ++i) {
       const elem = document.querySelector(translate_info[i]["elem"]);
       elem.innerHTML = translate_info[i]["text"];
-      alert(JSON.stringify(translate_info));
     }
   }
 };
