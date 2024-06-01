0d9c08019f56b02033065e14eb7de50062428aa7
The Infinitys <137460810+The-Infinitys@users.noreply.github.com>
2024-06-01 11:11:14 +0900

    修正

--- The-Infinitys-Infinity/The-Infinitys.js
+++ The-Infinitys-Infinity/The-Infinitys.js
@@ -326,10 +326,7 @@ const generate_honeycomb = () => {
     left:0;
     z-index:-1000;
     object-fit:fill;
-    animation-name: show, hue-rotation;
-    animation-duration: 5s, 5s;
-    animation-timing-function: linear, linear;
-    animation-iteration-count: initial, infinite;
+    animation: show 5s linear initial, hue-rotation 5s linear infinite;
   `;
   rainbow_svg.alt = "";
   document.body.appendChild(rainbow_svg);
