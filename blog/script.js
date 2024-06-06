a1ffadae3db4f677f2300fb8b35370e95cfedae4
The Infinitys <137460810+The-Infinitys@users.noreply.github.com>
2024-06-06 15:31:10 +0900

    プログラムの修正

--- blog/script.js
+++ blog/script.js
@@ -44,34 +44,9 @@ const getData = function (name) {
     }).catch((err) => console.log(`データが取得できませんでした：${err}`));
 };
 function blog_open(url) {
-  const blog_window = document.querySelector("#blog");
-  blog_window.style.opacity = "1";
-  blog_window.style.top="0";
-  document.querySelector("#loading-infinity-blog-view").style.visibility = "visible";
-  document.querySelector("#blog-view").src = url;
-  blog_window.classList.remove("blog-window-hide");
-  blog_window.classList.add("blog-window-show");
-}
-function blog_close() {
-  const blog_window = document.querySelector("#blog");
-  blog_window.classList.add("blog-window-hide");
-  blog_window.classList.remove("blog-window-show");
-  blog_window.style.opacity = "0";
-  blog_window.style.top="100vh";
+  window.location.href = url;
 }
 for (let i = data_list_length; i > 0; i--) {
   const pathname = "/api/blog/" + (blog_start.year + ~~((blog_start.month + i - 1) / 12)).toString() + "-" + ((blog_start.month + i - 2) % 12 + 1).toString()
   getData(pathname);
-}
-document.querySelector("#blog-copy").addEventListener("click", () => {
-  copyToClipboard("https://" + blog_domain + "/blog/?name=" + param_open);
-});
-function copyToClipboard(text) {
-  if (navigator.clipboard) {
-    return navigator.clipboard.writeText(text).then(function () {
-      alert("copied!")
-    })
-  } else {
-    alert(text)
-  }
 }
