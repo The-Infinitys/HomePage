1f0cf8ddcb237d490bfd11e4e5100070cf061b0e
The Infinity's <137460810+The-Infinitys@users.noreply.github.com>
2024-06-19 12:25:59 +0900

    修正

--- blog/script.js
+++ blog/script.js
@@ -44,21 +44,25 @@ const getData = (name) => {
     }).catch((err) => console.log(`データが取得できませんでした：${err}`));
 };
 //loading buttonが押されたのをキーに一気に読み込む。これはSEO対策ように実装しました。
-start_loading_button.onclick = () => {
-  for (let load_count = data_list_length; load_count > 0; load_count--) {
-    const pathname = "/article-" + blog_start.year.toString() + "/index/" + (blog_start.year + ~~((blog_start.month + load_count - 1) / 12)).toString() + "-" + ((blog_start.month + load_count - 2) % 12 + 1).toString()
-    getData(pathname);
-    if (load_count <= 1) {
-      start_loading_button.style.opacity = 0;
-      start_loading_button.classList.remove("show-blog-button");
-      start_loading_button.classList.add("hide-blog-button");
+let load_done = false;
+document.body.onclick = () => {
+  if (!load_done) {
+    load_done = true;
+    for (let load_count = data_list_length; load_count > 0; load_count--) {
+      const pathname = "/article-" + blog_start.year.toString() + "/index/" + (blog_start.year + ~~((blog_start.month + load_count - 1) / 12)).toString() + "-" + ((blog_start.month + load_count - 2) % 12 + 1).toString()
+      getData(pathname);
+      // if (load_count <= 1) {
+      //   start_loading_button.style.opacity = 0;
+      //   start_loading_button.classList.remove("show-blog-button");
+      //   start_loading_button.classList.add("hide-blog-button");
+      // }
     }
   }
 }
 //検索機能
 const article_search_input = document.querySelector("#list-head div input");
 const search_articles = () => {
-  const search_query = article_search_input.value.replace("　"," ").toLowerCase();
+  const search_query = article_search_input.value.replace("　", " ").toLowerCase();
   const blog_buttons = document.querySelector("#blog-button-section").children;
   if (search_query == "") {
     for (let i = 0; i < blog_buttons.length; i++) {
