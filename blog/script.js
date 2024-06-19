0934f2d53d6dc6cbb61c6b6fd67b1212cba49a69
The Infinity's <137460810+The-Infinitys@users.noreply.github.com>
2024-06-19 11:42:07 +0900

    修正

--- blog/script.js
+++ blog/script.js
@@ -56,17 +56,26 @@ start_loading_button.onclick = () => {
 //検索機能の導入
 const article_search_input = document.querySelector("#list-head input");
 const search_articles = () => {
-  const search_querys = article_search_input.value.split(" ");
+  const search_query = article_search_input.value;
   const blog_buttons = document.querySelector("#blog-button-section").children;
+  if (search_query == "") {
+    for (let i = 0; i < blog_buttons.length; i++) {
+      blog_buttons[i].style.display = "";
+    }
+    return null;
+  }
+  const search_querys = search_query.split(" ");
   for (let i = 0; i < blog_buttons.length; i++) {
     const blog_button = blog_buttons[i];
     const button_inner = blog_button.innerHTML;
     const title = button_inner.substring(button_inner.indexOf("<div>") + 5, button_inner.indexOf("</div>"));
     console.log(blog_button.style.display);
+    blog_button.style.display = "";
     for (let j = 0; j < search_querys.length; ++j) {
-      if (-1==title.indexOf(search_querys[j])){
-        blog_button.style.display="none";
+      if (-1 == title.indexOf(search_querys[j])) {
+        blog_button.style.display = "none";
       }
     }
   }
+  return search_query;
 }
