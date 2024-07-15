38b3617f66c72582c83e6701d6fa38b559d18e97
The-Infinitys <theinfinitysinfinity@gmail.com>
2024-07-15 10:10:07 +0000

    articleの完成。移行処理の開始

---
+++ article/script.js
@@ -0,0 +1,63 @@
+const recommendArticles = async () => {
+  const article_list = document.querySelector(".article-list-main");
+  article_list.innerHTML = "";
+  article_info_datas = [];
+  const add_article_button = (article_info) => {
+    const article_button = document.createElement("button");
+    const article_root_path =
+      "../article-" +
+      article_info.year.toString() +
+      "/" +
+      (article_info.month + 1).toString().padStart(2, "0") +
+      "/" +
+      article_info.id +
+      "/";
+    article_button.onclick = () => {
+      window.location.href = article_root_path;
+    };
+    const thumbnail = new Image();
+    if (article_info.thumbnail == "") {
+      thumbnail.src =
+        "../InfinitySpirit-blog/InfinitySpirit/template/image/loading.svg";
+    } else {
+      thumbnail.src = article_root_path + article_info.thumbnail;
+    }
+    const loading = new Image();
+    loading.src =
+      "../InfinitySpirit-blog/InfinitySpirit/template/image/loading.svg";
+    const title = document.createElement("div");
+    title.innerHTML =
+      "<h1>" + article_info.title + "</h1><p>date: " + article_info.date;
+    article_button.append(loading);
+    article_button.append(thumbnail);
+    article_button.append(title);
+    article_list.append(article_button);
+  };
+  for (
+    let year_count = new Date().getFullYear();
+    year_count >= 2024;
+    year_count--
+  ) {
+    for (let month_count = 0; month_count < 12; month_count++) {
+      const list_path =
+        "../article-" +
+        year_count.toString() +
+        "/" +
+        (1 + ((12- month_count) % 12))
+          .toString()
+          .padStart(2, "0") +
+        "/articles.json";
+      await fetch(list_path)
+        .then((res) => res.json())
+        .then((article_data) => {
+          const datas = article_data.articles;
+          datas.forEach((article_info) => {
+            article_info.month = (12 - month_count) % 12;
+            article_info.year = year_count;
+            add_article_button(article_info);
+          });
+        });
+    }
+  }
+};
+recommendArticles();
