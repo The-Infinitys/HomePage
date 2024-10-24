85b78d00007931e72772dac59cb958553dba92cf
The Infinity's <137460810+The-Infinitys@users.noreply.github.com>
2024-10-24 15:06:31 +0900

    かっくいい

--- background/space-test/script.ts
+++ background/space-test/script.ts
@@ -1,1 +1,38 @@
-const elems: HTMLElement = document.querySelectorAll(".bg>div.three-d");
+const run: Function = () => {
+  const space_parent: HTMLElement = document.querySelector(".bg");
+  const asteroid_count: number = 20;
+  const asteroid_dispersion: number = asteroid_count / 10;
+  const generate: Function = () => {
+    const asteroid: HTMLElement = document.createElement("div");
+    asteroid.className = "light three-d";
+    space_parent!.append(asteroid);
+  };
+  const define_animation: Function = () => {
+    const elems: HTMLElement[] = space_parent!.children;
+    for (let i = 0; i < elems.length; i++) {
+      const elem: HTMLElement = elems[i];
+      elem.addEventListener("animationiteration", () => {
+        elem.setAttribute(
+          "style",
+          `
+          --x-pos: ${(
+            asteroid_dispersion *
+            (2 * Math.random() - 1)
+          ).toString()};
+          --y-pos: ${(
+            asteroid_dispersion *
+            (2 * Math.random() - 1)
+          ).toString()};
+          background-color: hsl(${Math.random().toString()}turn, 100%, 50%);
+          `
+        );
+      });
+      console.log(elems.length);
+    }
+  };
+  for (let i = 0; i < asteroid_count; i++) {
+    setTimeout(generate, (i * 2000) / asteroid_count);
+  }
+  setTimeout(define_animation, 2000);
+};
+run();
